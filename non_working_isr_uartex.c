bd8440697f93a85873d01e4110c57ec22255bc0a
Subharthi Banerjee <sbanerjee15@huskers.unl.edu>
2019-09-16 17:59:15 -0500

    Update non_working_isr_uartex.c

--- non_working_isr_uartex.c
+++ non_working_isr_uartex.c
@@ -12,17 +12,17 @@
 */
 volatile unsigned char received_byte=0; // should I use static or volatile here?
 
-volatile unsigned char recieved_flag = 0;
+volatile unsigned char received_flag = 0;
 // check .rst file after compilation: what is in IVT
 // there can be another trick used
-void char ISR_receive() __interrupt (4) {
+void ISR_receive() __interrupt (4) {
 
 	 
 
 	if (RI == 1){
 		received_byte = SBUF;
 		RI = 0;
-		recieved_flag= 1;
+		received_flag= 1;
 
 	}
 	
@@ -33,7 +33,7 @@ void char ISR_receive() __interrupt (4) {
 
 void UART_Init(){
 
-	SCON = 0x50;  // Asynchronous mode, 8-bit data and 1-stop bit
+    SCON = 0x50;  // Asynchronous mode, 8-bit data and 1-stop bit
     TMOD = 0x20;  // Timer1 in Mode2. in 8 bit auto reload
     TH1 =  0xFD;  // Load timer value for 9600 baudrate
     TR1 = 1;      // Turn ON the timer for Baud rate generation
